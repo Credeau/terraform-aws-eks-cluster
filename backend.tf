970401a63764a38546b6135e57ef273840e7a358
bhardwajashutosh077 <bhardwajashutosh077@gmail.com>
2025-08-14 11:57:30 +0530

    Restructure

--- backend.tf
+++
@@ -1,8 +0,0 @@
-# Configure the remote backend here
-terraform {
-  backend "s3" {
-    bucket = "credeautest-terraform-states"          # change this as per the AWS account
-    key    = "di_eks_cluster/prod/terraform.tfstate" # do not touch this!
-    region = "ap-south-1"
-  }
-}
